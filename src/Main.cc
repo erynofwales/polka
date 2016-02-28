a7d0607c924e8cb329eca96aa47b2ea4b5f83948
Eryn Wells <eryn@erynwells.me>
2016-02-28 13:33:20 -0500

    Okay, just create a console object when you need one... for now

--- src/Main.cc
+++ src/Main.cc
@@ -14,38 +14,30 @@
 extern "C"
 void
 kearly()
-{ }
-
-
-/** The beginning of the world... */
-extern "C"
-void
-kmain()
 {
-    auto console = kernel::Console::systemConsole();
+    kernel::Console console;
     console.clear(kernel::Console::Color::Blue);
-    console.writeString("Hello world!\n");
+    console.writeString("Loading system ...\n");
 
-    /*
-     * TODO: The performance of this loop slowed down a _lot_ (7 to 8 orders of
-     * magnitude according to the busy loop) when I moved this code from
-     * kearly() to here. I wonder if it has something to do with object
-     * initialization? Should probably investigate some.
-     */
     volatile int foo = 0;
-    int j = 0;
     int i = 0;
     for (;;) {
-        if (j == 0) {
+        if (i == 0) {
             console.writeString("--- MARK ---\n");
         }
         console.writeChar('a' + i);
         console.writeChar('\n');
         i = (i + 1) % 26;
-        j = (j + 1) % 500;
 
         for (uint32_t k = 0; k < (2u << 20) - 1; k++) {
             foo /= 2;
         }
     }
 }
+
+
+/** The beginning of the world... */
+extern "C"
+void
+kmain()
+{ }
