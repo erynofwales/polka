b893151fa4f400f97279f50d83dee091c4c1b065
Eryn Wells <eryn@erynwells.me>
2016-02-29 22:04:38 -0500

    Header formatting

--- src/Descriptors.hh
+++ src/Descriptors.hh
@@ -1,4 +1,6 @@
-/* Descriptors.hh vim: set tw=80: Eryn Wells <eryn@erynwells.me>
+/* Descriptors.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
  */
 /**
  * Declaration of relevant classes and functions for dealing with the GDT and
