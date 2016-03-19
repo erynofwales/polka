fad5a48e5b7a002563d5d1503bfff5087bb65ee6
Eryn Wells <eryn@erynwells.me>
2016-03-19 04:05:23 -0400

    Add compiler Attributes header file

---
+++ src/Attributes.hh
@@ -0,0 +1,17 @@
+/* Attributes.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+/**
+ * Defines for common compiler attributes.
+ *
+ * See https://gcc.gnu.org/onlinedocs/gcc/Type-Attributes.html for GCC
+ * attributes.
+ */
+
+#ifndef __ATTRIBUTES_HH__
+#define __ATTRIBUTES_HH__
+
+#define PACKED __attribute__((packed))
+
+#endif /* __ATTRIBUTES_HH__ */
