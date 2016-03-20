addf61ee7a299c063bd180f50d0082ba5e2e69a3
Eryn Wells <eryn@erynwells.me>
2016-03-20 03:38:12 -0400

    Add typedefs for standard types
    Rust style integer type names. So much easier to write out...

---
+++ src/kstd/Types.hh
@@ -0,0 +1,31 @@
+/* Types.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+/**
+ * File description.
+ */
+
+#ifndef __KSTD_TYPES_HH__
+#define __KSTD_TYPES_HH__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef __UINT8_TYPE__  u8;
+typedef __INT8_TYPE__   i8;
+typedef __UINT16_TYPE__ u16;
+typedef __INT16_TYPE__  i16;
+typedef __UINT32_TYPE__ u32;
+typedef __INT32_TYPE__  i32;
+typedef __UINT64_TYPE__ u64;
+typedef __INT64_TYPE__  i64;
+
+typedef __SIZE_TYPE__ usize;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __KSTD_TYPES_HH__ */
