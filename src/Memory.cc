b9ece9bbcb376f4e36fe13ccb261fcfff8da8e7b
Eryn Wells <eryn@erynwells.me>
2016-03-25 01:21:37 -0400

    Add MemoryManager class
    This will be the top-level class for the memory manager subsystem.
    Copy in some code from kmain()

---
+++ src/Memory.cc
@@ -0,0 +1,37 @@
+/* Memory.cc
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+/**
+ * Top-level classes for managing system memory.
+ */
+
+#include "Memory.hh"
+
+namespace kernel {
+
+/*
+ * Public
+ */
+
+void
+MemoryManager::initialize(Console& console)
+{
+    initializeGDT();
+    console.printString("GDT loaded\n");
+}
+
+/*
+ * Private
+ */
+
+void
+MemoryManager::initializeGDT()
+{
+    mGDT.setNullDescriptor(0);
+    mGDT.setDescriptor(1, x86::GDT::DescriptorSpec::kernelSegment(0, 0xFFFFFFFF, x86::GDT::Type::CodeEXR));
+    mGDT.setDescriptor(2, x86::GDT::DescriptorSpec::kernelSegment(0, 0xFFFFFFFF, x86::GDT::Type::DataRW));
+    mGDT.load();
+}
+    
+} /* namespace kernel */
---
+++ src/Memory.hh
@@ -0,0 +1,30 @@
+/* Memory.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+/**
+ * Declaration of top-level classes for managing system memory.
+ */
+
+#ifndef __MEMORY_HH__
+#define __MEMORY_HH__
+
+#include "Console.hh"
+#include "Descriptors.hh"
+
+
+namespace kernel {
+
+struct MemoryManager
+{
+    void initialize(Console& console);
+
+private:
+    x86::GDT mGDT;
+
+    void initializeGDT();
+};
+    
+} /* namespace kernel */
+
+#endif /* __MEMORY_HH__ */
