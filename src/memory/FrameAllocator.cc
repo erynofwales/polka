35997c61b65c44fc7d5654948f1615d22f672c58
Eryn Wells <eryn@erynwells.me>
2016-04-03 20:02:34 -0400

    Barebones for a FrameAllocator class

---
+++ src/memory/FrameAllocator.cc
@@ -0,0 +1,15 @@
+/* FrameAllocator.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+/**
+ * An object to tracks and allocate physical page frames.
+ */
+
+#include "FrameAllocator.hh"
+
+namespace kernel {
+
+
+    
+} /* namespace kernel */
---
+++ src/memory/FrameAllocator.hh
@@ -0,0 +1,21 @@
+/* FrameAllocator.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+/**
+ * An object to tracks and allocate physical page frames.
+ */
+
+#ifndef __MEMORY_FRAMEALLOCATOR_HH__
+#define __MEMORY_FRAMEALLOCATOR_HH__
+
+namespace kernel {
+
+struct FrameAllocator
+{
+};
+
+} /* namespace  */
+
+#endif /* __MEMORY_FRAMEALLOCATOR_HH__ */
+
