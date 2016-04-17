e58c29e15da9dcb8b6803b5baaa7a2d306c814f4
Eryn Wells <eryn@erynwells.me>
2016-04-17 16:49:24 -0400

    Define pointer integer types

--- src/kstd/Types.hh
+++ src/kstd/Types.hh
@@ -13,6 +13,8 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
 typedef __UINT8_TYPE__  u8;
 typedef __INT8_TYPE__   i8;
 typedef __UINT16_TYPE__ u16;
@@ -21,8 +23,15 @@ typedef __UINT32_TYPE__ u32;
 typedef __INT32_TYPE__  i32;
 typedef __UINT64_TYPE__ u64;
 typedef __INT64_TYPE__  i64;
-
 typedef __SIZE_TYPE__ usize;
+
+#if __WORDSIZE == 64
+typedef i64 iptr;
+typedef u64 uptr;
+#else
+typedef i32 iptr;
+typedef u32 uptr;
+#endif
 
 #ifdef __cplusplus
 }
